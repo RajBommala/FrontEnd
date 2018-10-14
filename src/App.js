600565ffba71f10951eb25359de419c9ed51b55f
Bommala <basusmy@gmail.com>
2018-10-14 01:03:18 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://limitless-brushlands-16071.herokuapp.com/imageurl', {
+      fetch('https://boiling-sierra-58311.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://limitless-brushlands-16071.herokuapp.com/image', {
+          fetch('https://boiling-sierra-58311.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://limitless-brushlands-16071.herokuapp.com/signin', {
+    fetch('https://boiling-sierra-58311.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
